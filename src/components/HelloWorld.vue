55ac9a52ee0e622cfbff0075ad4974a580e429bb
shashank kullkarni <shashankkulkarni80425@gmail.com>
2021-08-05 22:19:06 +0530

    implemented api axios api call and stoed api data in to indexed db and retrive that data in alert

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,108 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<template><div>
+   <h1>Indexed Db</h1>
+  <!-- <button @click="createDb">Create db</button> -->
+  <!-- <button @click="addData">add data</button> -->
+  <button @click="retriveData">read all</button>
+  <!-- <button @click="read">read</button> -->
+  <!-- <button @click="remove">remove</button> -->
+  <!-- <h1>{{productData}}</h1> -->
+  <h1>{{aryofObjects}}</h1>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+   name:'indexedDb',
+   data(){
+      return{
+         db:"",
+         request:"",
+         productData:"",
+         aryofObjects:[]
+      }
+   },
+   created(){
+       window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+
+         window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange
+
+         if(!window.indexedDB){
+            window.alert("Your browser does not support a indexed Db");
+         }
+         // const employeeData = [ 
+         //    { id: "00-01", name: "shashank", age: 23, email: "shashank@gmail.com" },
+         //    { id: "00-02", name: "utkarsh;", age: 32, email: "utk@gamil.com.com" }
+         // ];
+          var url = "https://fakestoreapi.com/products/category/"+"jewelery" ;
+          axios.get(url)
+          .then((resp)=>{
+
+              this.request = window.indexedDB.open("myDb",1);
+         this.request.onerror = function(){
+            console.log("error");
+         }
+         // this.request.onsuccess = function(){
+         //     this.db= this.request.result;
+           
+         //    // console.log("success: " +this.db);
+         // }
+         this.request.onupgradeneeded = function (event) {
+             var db1= event.target.result;
+            var objectStore = db1.createObjectStore("Products",{keyPath: "id"});
+
+             
+            this.productData=resp.data; 
+            console.log(this.productData);   
+             for(var i in this.productData){
+               objectStore.add(this.productData[i]);
+            }
+         }
+         console.log("data stored sucessfully");
+        console.log("created sucessfully")
+         console.log(this.db);
+        })
+
+        
+
+        
+           
+
+   },
+   methods:{
+      retriveData(){
+          window.IDBTransaction = window.IDBTransaction || 
+         window.webkitIDBTransaction || window.msIDBTransaction;
+
+         
+ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+         window.msIDBKeyRange
+
+         this.db = this.request.result;
+         // var transcation = this.db.tansaction("employee");
+         // var objectStore = this.db.tansaction("employee").objectStore("employee");
+         this.request = this.db.transaction(["Products"],"readwrite")
+         .objectStore("Products").openCursor();
+         this.request.onsuccess = function(event){
+            console.log(event);
+            var cursor = event.target.result;
+            if(cursor){
+               alert("Name for id " + cursor.key + "is" + cursor.value.title+ "Age:" + cursor.value.category + "description :" + cursor.value.description) + "image : "+ cursor.value.image + "price :"+cursor.value.price;
+               cursor.continue();
+            }
+         }
+         
+         alert("Read All Method")
+      },      
+   }
+   
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
